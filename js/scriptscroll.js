ef7a7cf4babc83d50e9daf66eaa9e4ca9a6b491d
Jasmin31000 <62843904+Jasmin31000@users.noreply.github.com>
2022-06-28 18:18:45 +0200

    maj

--- js/scriptscroll.js
+++ js/scriptscroll.js
@@ -2,8 +2,8 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar").style.top = "40px";
   } else {
-    document.getElementById("navbar").style.top = "-50px";
+    document.getElementById("navbar").style.top = "0px";
   }
 }
