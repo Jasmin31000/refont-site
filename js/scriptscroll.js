dffabe7ec9c4d654538df9e35cd6a10c3b4ff565
Jasmin31000 <62843904+Jasmin31000@users.noreply.github.com>
2022-06-29 11:03:46 +0200

    mise en place du scroll

--- js/scriptscroll.js
+++ js/scriptscroll.js
@@ -2,8 +2,14 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-    document.getElementById("navbar").style.top = "40px";
+    document.getElementById("navbar").style.top = "0px";
+    document.getElementById("navbar").style.position = "fixed";
+    document.getElementById("navbar").style.height = "150px";
+    document.getElementById("indica").style.top = "5px";
   } else {
     document.getElementById("navbar").style.top = "0px";
+    document.getElementById("navbar").style.position = "relative";
+    document.getElementById("navbar").style.height = "73px";
+    document.getElementById("indica").style.top = "-35px";
   }
 }
