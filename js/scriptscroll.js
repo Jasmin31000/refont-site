0513f810f5c40a7483fdbcb16801d560528938c6
Jasmin31000 <62843904+Jasmin31000@users.noreply.github.com>
2022-07-01 14:02:51 +0200

    maj

--- js/scriptscroll.js
+++ js/scriptscroll.js
@@ -1,15 +1,13 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
+  let navigbar = document.getElementById("navbar");
+  let indicbar = document.getElementById("indica");
   if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-    document.getElementById("navbar").style.top = "0px";
-    document.getElementById("navbar").style.position = "fixed";
-    document.getElementById("navbar").style.height = "150px";
-    document.getElementById("indica").style.top = "5px";
+    navigbar.classList.replace("bnav", "bnavscroll");
+    indicbar.classList.replace("indic", "indicscroll");
   } else {
-    document.getElementById("navbar").style.top = "0px";
-    document.getElementById("navbar").style.position = "relative";
-    document.getElementById("navbar").style.height = "73px";
-    document.getElementById("indica").style.top = "-35px";
+    navigbar.classList.replace("bnavscroll", "bnav");
+    indicbar.classList.replace("indicscroll", "indic");
   }
 }
